52054788fd82df64c082acf0c8bb3c03b037b689
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-10 00:31:11 +0700

    remove 3rd party [cc]

--- resources/[3rd]/cc-rpchat/client/main.lua
+++
@@ -1,7 +0,0 @@
-local ccChat = exports['cc-chat']
-
-RegisterNetEvent('cc-rpchat:addMessage')
-AddEventHandler('cc-rpchat:addMessage', function(color, icon, subtitle, msg)
-    timestamp = ccChat:getTimestamp()
-    TriggerEvent('chat:addMessage', { templateId = 'ccChat', multiline = false, args = { color, icon, subtitle, timestamp, msg } })
-end)
--- resources/[3rd]/cc-rpchat/config.lua
+++
@@ -1,3 +0,0 @@
-config = {}
-
-config.identity = false         --If you are using esx_identity then make it true to get rp names on chat
