97f436099b280a0e32345a4cb7b34c6e0d62d6e4
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-09 22:07:40 +0700

    edit jxw_playerinfo

--- resources/[joxuwa]/jxw_playerinfo/client/client.lua
+++ resources/[joxuwa]/jxw_playerinfo/client/client.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function()
     local z_key = 48
     while true do
         Citizen.Wait(1)
-        if IsControlJustReleased(1, z_key) then
+        if IsControlJustPressed(5, z_key) then
             TriggerEvent('chatMessage', '', {255, 0, 0}, 'Z Key Pressed')
         end
     end
