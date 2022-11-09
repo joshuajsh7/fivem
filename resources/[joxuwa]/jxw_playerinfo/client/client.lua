9dae14bc52c991a0b28b5860b5e32147e36d0b31
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-09 22:27:22 +0700

    edit jxw_playerinfo

--- resources/[joxuwa]/jxw_playerinfo/client/client.lua
+++ resources/[joxuwa]/jxw_playerinfo/client/client.lua
@@ -2,9 +2,9 @@ Citizen.CreateThread(function()
     local z_key = 48
     while true do
         Citizen.Wait(1)
-        if IsControlJustPressed(5, z_key) then
-            local len = reaper.time_precise()
-            TriggerEvent('chatMessage', '', {255, 0, 0}, len)
+        if IsControlJustPressed(0, z_key) then
+            t = os.date('*t')
+            TriggerEvent('chatMessage', '', {255, 0, 0}, t.sec)
         end
     end
 end)
--- resources/[joxuwa]/jxw_playerinfo/fxmanifest.lua
+++ resources/[joxuwa]/jxw_playerinfo/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Joxuwa'
-description 'Show Player Info (ID)'
+description 'Show player info (ID)'
 version '1.0.0'
 
 client_script 'client/client.lua'
