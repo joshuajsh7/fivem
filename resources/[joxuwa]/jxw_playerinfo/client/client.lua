ea79e70a94695227b8c43a83d5c137598e31f3d0
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-09 22:30:07 +0700

    jxw_playerinfo

--- resources/[joxuwa]/jxw_playerinfo/client/client.lua
+++ resources/[joxuwa]/jxw_playerinfo/client/client.lua
@@ -3,8 +3,11 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1)
         if IsControlJustPressed(0, z_key) then
-            t = os.date('*t')
-            TriggerEvent('chatMessage', '', {255, 0, 0}, t.sec)
+            local timestamp = os.time()
+            local d = os.date('*t', timestamp).wday
+            local h = tonumber(os.date('%H', timestamp))
+            local m = tonumber(os.date('%M', timestamp))
+            TriggerEvent('chatMessage', '', {255, 0, 0}, m)
         end
     end
 end)
