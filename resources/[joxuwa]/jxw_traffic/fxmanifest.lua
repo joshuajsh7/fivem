cfb33c100a1c98de3ec6c96e5a1ca07578012f7b
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-09 23:35:27 +0700

    change jxw_nonpc to jxw_traffic

--- resources/[joxuwa]/jxw_traffic/fxmanifest.lua
+++ resources/[joxuwa]/jxw_traffic/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Joxuwa'
-description 'Setting npc / ai in server'
+description 'Setting npc / ai / traffic in server'
 version '1.0.0'
 
 client_script 'client/client.lua'
