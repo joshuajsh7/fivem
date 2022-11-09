49d14d8e95891ec86a669f20873b363c777a42f7
joshuajsh7 <77525318+joshuajsh7@users.noreply.github.com>
2022-11-09 22:00:40 +0700

    jxw_playerinfo

--- resources/[joxuwa]/jxw_playerinfo/client/client.lua
+++ resources/[joxuwa]/jxw_playerinfo/client/client.lua
@@ -2,6 +2,8 @@ Citizen.CreateThread(function()
     local z_key = 48
     while true do
         Citizen.Wait(1)
-        print('Why are you still dead?')
+        if IsControlJustReleased(1, z_key) then
+            TrigerEvent('chatMessage', '', {255, 0, 0}, ESX.GetPlayerData().identifier)
+        end
     end
 end)
